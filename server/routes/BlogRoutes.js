da97301b2c1a08496e9a52c3a8c7233e03bf0872
Satwik Biswas <148995066+satwikcoder007@users.noreply.github.com>
2024-03-06 19:33:45 +0000

    follwer controller added

--- server/routes/BlogRoutes.js
+++ server/routes/BlogRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 const blogRoute = express.Router();
 import { CreateBlog ,UpdateBlog, LikeBlog,CommentBlog } from "../controllers/blogController.js";
+import { FollowPerson } from "../controllers/followController.js";
 import auth from "../middlewares/auth.js"
 
+//blog posting route
 blogRoute.post("/createblog",auth,CreateBlog);
 blogRoute.post("/updateblog",auth,UpdateBlog);
 blogRoute.post("/likeblog",auth,LikeBlog);
 blogRoute.post("/commentblog",auth,CommentBlog);
 
+//following route
+blogRoute.post("/follow",auth,FollowPerson);
 
 export default blogRoute;
