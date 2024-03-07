d8dade21c579ebe8984d553405dd794fd38b1ca5
Satwik Biswas <148995066+satwikcoder007@users.noreply.github.com>
2024-03-07 18:16:48 +0000

    blog image part added

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Login from './components/Login'
 import BlogDescription from './blogComponents.jsx/BlogDescription'
+import BlogUploadImage from './blogComponents.jsx/BlogUploadImage'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -12,6 +13,7 @@ function App() {
        <Route path='/login' element={<Login></Login>}/>
        <Route path='/' element={<Navbar/>}/>
        <Route path="/blog" element={<BlogDescription/>}/>
+       <Route path="/image" element={<BlogUploadImage/>}/>
     </Routes>
 
     </>
