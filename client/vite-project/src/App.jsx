22ea6497fc21b71aedfc827abbaaa7ba2cb38e67
Pratip2004 <157035698+Pratip2004@users.noreply.github.com>
2024-03-08 11:21:52 +0530

    Login and register page is done

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/navbar'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Login from './components/Login'
+import Register from './components/Register'
 import BlogDescription from './blogComponents.jsx/BlogDescription'
 import BlogUploadImage from './blogComponents.jsx/BlogUploadImage'
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <>
     <Routes>
        <Route path='/login' element={<Login></Login>}/>
+       <Route path='/register' element={<Register></Register>}/>
        <Route path='/' element={<Navbar/>}/>
        <Route path="/blog" element={<BlogDescription/>}/>
        <Route path="/image" element={<BlogUploadImage/>}/>
