31b384dded398698f97aab9e810b2099f7f98a9a
Ryzen_10 <115201346+Dipu010@users.noreply.github.com>
2024-03-10 11:39:06 +0530

    Small Bugs fixed in Login and navbar

--- client/vite-project/src/components/navbar.jsx
+++ client/vite-project/src/components/navbar.jsx
@@ -10,30 +10,30 @@ function Navbar() {
    {/* <!-- component -->
 <!-- follow me on twitter @asad_codes --> */}
 
-<div class="flex flex-wrap ">
-  <section class="relative mx-auto">
+<div className="flex flex-wrap ">
+  <section className="relative mx-auto">
       {/* <!-- navbar --> */}
-    <nav class="flex justify-between bg-gray-900 text-white w-screen">
-      <div class="px-5 xl:px-12 py-6 flex w-full items-center">
-        <a class="text-3xl font-bold font-heading" href="#">
-           <img class="h-9" src="logo.png" alt=""/> 
+    <nav className="flex justify-between bg-gray-900 text-white w-screen">
+      <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+        <a className="text-3xl font-bold font-heading" href="#">
+           <img className="h-9" src="logo.png" alt=""/> 
           Logo Here.
         </a>
         {/* <!-- Nav Links --> */}
-        <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-          <li><a class="hover:text-gray-200" href="#">Home</a></li>
-          <li><a class="hover:text-gray-200" href="#">Catagory</a></li>
-          <li><a class="hover:text-gray-200" href="#">Collections</a></li>
-          <li><a class="hover:text-gray-200" href="#">Contact Us</a></li>
+        <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+          <li><a className="hover:text-gray-200" href="#">Home</a></li>
+          <li><a className="hover:text-gray-200" href="#">Catagory</a></li>
+          <li><a className="hover:text-gray-200" href="#">Collections</a></li>
+          <li><a className="hover:text-gray-200" href="#">Contact Us</a></li>
         </ul>
         {/* <!-- Header Icons --> */}
-        <div class="hidden xl:flex space-x-5 items-center">
+        <div className="hidden xl:flex space-x-5 items-center">
           {/* <a class="hover:text-gray-200" href="#">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
             </svg>
           </a> */}
-          <Link to='/register'> <button class="h-8 px-3 rounded-md shadow text-white bg-blue-500">Sign-UP</button></Link>
+          <Link to='/register'> <button className="h-8 px-3 rounded-md shadow text-white bg-blue-500">Sign-UP</button></Link>
           {/* <a class="flex items-center hover:text-gray-200" href="#">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
