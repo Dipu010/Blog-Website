090fb00683442a32d558fc920aa6d5d04e2d03d9
Satwik Biswas <148995066+satwikcoder007@users.noreply.github.com>
2024-03-16 00:50:54 +0530

    comment is displayed

--- client/vite-project/src/blogRenderComponents/BlogOwner.jsx
+++ client/vite-project/src/blogRenderComponents/BlogOwner.jsx
@@ -8,7 +8,7 @@ export default function BlogOwner(props) {
   return (
     <div className=" flex justify-center" >
       <div className=" box-border rounded-full bg-slate-500 h-[60px] w-[60px] flex justify-center items-center cursor-pointer">
-        <IoMdPerson className=" rounded-full text-[40px] text-white" />
+        <img src={props.profilePicture} className=" object-contain rounded-full"/>
       </div>
       <div className=" flex flex-col ml-[15px] ">
         <div className=" font-semibold cursor-pointer text-[20px] text-white" onClick={()=>{navigate('/profile')}}>{props.firstName} {props.lastName}</div>
--- client/vite-project/src/blogRenderComponents/ShowComments.jsx
+++ client/vite-project/src/blogRenderComponents/ShowComments.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CommentStructure from "./CommentStructure";
 export default function ShowComments(props) {
   const [loading, setLoading] = useState(1);
   const [commentArray, setCommentArray] = useState([]);
@@ -25,7 +26,7 @@ export default function ShowComments(props) {
         <div className="flex items-center space-x-2">
           <div className="animate-pulse rounded-full bg-gray-500 h-12 w-12 "></div>
           <div className="space-y-2">
-            <div class="animate-pulse rounded-md bg-gray-500 h-4 w-[200px]">
+            <div className="animate-pulse rounded-md bg-gray-500 h-4 w-[200px]">
               {" "}
             </div>
             <div className="animate-pulse rounded-md bg-gray-500 h-4 w-[170px]">
@@ -34,8 +35,21 @@ export default function ShowComments(props) {
           </div>
         </div>
       ) : (
-        <div>
-            
+        <div className=" mt-[40px] flex flex-col gap-6">
+          {commentArray.map((val) => {
+            return (
+              <CommentStructure
+                description={val.description}
+                profilePicture={val.user.profilePicture}
+                firstName={val.user.firstName}
+                key={val._id}
+                lastName={val.user.lastName}
+              />
+            );
+          })}
+          <button className=" box-border bg-inherit px-2 w-[180px] rounded-md py-1 hover:bg-slate-500 cursor-pointer text-white font-semibold">
+            Load More Comments
+          </button>
         </div>
       )}
     </>
